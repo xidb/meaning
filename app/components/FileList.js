82599f3db2d8baf0b91e40bd7912f8e15332a70a
Dmitry Boytsov <boytsov.dm@gmail.com>
2018-01-14 17:06:45 +0300

    Add react-table and style it, handle row count change on resize

--- app/components/FileList.js
+++ app/components/FileList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { DropTarget } from 'react-dnd';
+import ReactTable from 'react-table';
 
 const boxTarget = {
 	drop(props, monitor) {
@@ -17,6 +18,13 @@ const boxTarget = {
 }))
 
 export default class FileList extends Component {
+	constructor() {
+		super();
+		this.state = {
+			rows: 23
+		}
+	}
+
 	static propTypes = {
 		connectDropTarget: PropTypes.func.isRequired,
 		isOver: PropTypes.bool.isRequired,
@@ -26,19 +34,76 @@ export default class FileList extends Component {
 		files: PropTypes.arrayOf(PropTypes.object),
 	};
 
-	list(files) {
-		return files.map(file => (
-			<div key={file.path}>{file.path} {file.title}</div>
-		));
+	componentWillMount() {
+		this.updateDimensions();
+		window.addEventListener('resize', this.updateDimensions.bind(this));
+	}
 
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateDimensions.bind(this));
+	}
+
+	updateDimensions() {
+		let containerHeight = window.innerHeight * 0.9;
+		const filelist = document.querySelector('.filelist');
+		if (filelist !== null) {
+			containerHeight = filelist.offsetHeight;
+		}
+		const tableHeight = containerHeight - 85 - 28.5; // pagination and table header height
+		const rowHeight = 33.5;
+		this.setState({ rows: Math.floor(tableHeight/rowHeight) });
 	}
 
 	render() {
 		const { canDrop, isOver, connectDropTarget } = this.props;
 		const { files } = this.props;
+		const { rows } = this.state;
+
+		const columns = [
+			{
+				Header: 'Artist',
+				accessor: 'artist'
+			},
+			{
+				Header: 'Year',
+				accessor: 'date',
+				maxWidth: 50
+			},
+			{
+				Header: 'Album',
+				accessor: 'album'
+			},
+			{
+				Header: 'Disc',
+				accessor: 'disc',
+				maxWidth: 50
+			},
+			{
+				Header: 'Track',
+				accessor: 'track',
+				maxWidth: 60
+			},
+			{
+				Header: 'Title',
+				accessor: 'title'
+			},
+		];
+
+		const sort = [
+			{id: 'album_artist'}, {id: 'year'}
+		];
 
 		return connectDropTarget(
-			<div className="filelist">{this.list(files)}</div>
+			<div className="filelist">
+				<ReactTable
+					data={files}
+					columns={columns}
+					showPageSizeOptions={false}
+					pageSize={rows}
+					defaultSorted={sort}
+					className="-striped -highlight"
+				/>
+			</div>
 		);
 	}
 }
