ee389ce324e1dd522d1c2517784cabbaaf8e55ed
Dmitry Boytsov <boytsov.dm@gmail.com>
2018-01-31 03:42:56 +0300

    Keyboard/mouse navigation!!!1!

--- app/components/FileList.js
+++ app/components/FileList.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 import { DropTarget } from 'react-dnd';
 import ReactTable from 'react-table';
 
+import { remote } from 'electron';
+import shortcut from 'electron-localshortcut';
+import _ from 'lodash/core';
+
 const boxTarget = {
 	drop(props, monitor) {
 		if (props.onDrop) {
@@ -24,8 +28,14 @@ export default class FileList extends Component {
 			page: 0,
 			rows: 24,
 			search: '',
-			selectedId: 0
+			selectedIndex: -1
 		};
+		this.lastPage = 9999999;
+		this.pageRows = {};
+		this.canNext = true;
+		this.startRow = 0;
+		this.endRow = 9999999;
+		this.lastIndex = 9999999;
 	}
 
 	static propTypes = {
@@ -37,6 +47,94 @@ export default class FileList extends Component {
 		files: PropTypes.arrayOf(PropTypes.object),
 	};
 
+	componentDidMount() {
+		this.win = remote.getCurrentWindow();
+
+		shortcut.register(this.win, 'Home', () => {
+			if (this.state.page !== 0) {
+				this.setState({page: 0})
+			}
+		});
+
+		shortcut.register(this.win, 'End', () => {
+			if (this.state.page !== this.lastPage) {
+				this.setState({page: this.lastPage})
+			}
+		});
+
+		const prevPage = () => {
+			if (this.state.page !== 0) {
+				this.setState({page: this.state.page - 1})
+			}
+		};
+
+		shortcut.register(this.win, 'Left', prevPage);
+		shortcut.register(this.win, 'PageUp', prevPage);
+
+		const nextPage = () => {
+			if (this.canNext) {
+				this.setState({page: this.state.page + 1})
+			}
+		};
+
+		shortcut.register(this.win, 'Right', nextPage);
+		shortcut.register(this.win, 'PageDown', nextPage);
+
+		const up = () => {
+			let toIndex = this.state.selectedIndex - 1;
+
+			if (!(toIndex >= this.startRow - 1 && toIndex <= this.endRow)) {
+				toIndex = Math.min(this.endRow - 1, this.lastIndex);
+			}
+
+			if (toIndex < 0) {
+				return;
+			}
+
+			if (toIndex >= this.startRow) {
+				this.setState({selectedIndex: toIndex});
+			} else {
+				this.setState({page: this.state.page - 1, selectedIndex: toIndex});
+			}
+
+			void this.props.songSelected(_.find(this.pageRows, {_index: toIndex})['_original']);
+		};
+
+		shortcut.register(this.win, 'Up', up);
+
+		const down = () => {
+			let toIndex = this.state.selectedIndex + 1;
+
+			if (!(toIndex >= this.startRow && toIndex <= this.endRow)) {
+				toIndex = this.startRow;
+			}
+
+			if (toIndex > this.lastIndex) {
+				return;
+			}
+
+			if (toIndex < this.endRow) {
+				this.setState({selectedIndex: toIndex});
+			} else {
+				this.setState({page: this.state.page + 1, selectedIndex: toIndex});
+			}
+
+			void this.props.songSelected(_.find(this.pageRows, {_index: toIndex})['_original']);
+		};
+
+		shortcut.register(this.win, 'Down', down);
+
+		const handleMouseWheel = (event) => {
+			if (event.deltaY > 0) {
+				down();
+			} else if (event.deltaY < 0) {
+				up();
+			}
+		};
+
+		window.addEventListener('wheel', handleMouseWheel, true);
+	}
+
 	componentWillMount() {
 		this.updateDimensions();
 		window.addEventListener('resize', this.updateDimensions.bind(this));
@@ -44,6 +142,7 @@ export default class FileList extends Component {
 
 	componentWillUnmount() {
 		window.removeEventListener('resize', this.updateDimensions.bind(this));
+		shortcut.unregisterAll(this.win);
 	}
 
 	updateDimensions() {
@@ -133,13 +232,22 @@ export default class FileList extends Component {
 				<ReactTable
 					data={files}
 					columns={columns}
+					getProps={(props) => {
+						this.lastPage = props.pages - 1;
+						this.pageRows = props.pageRows;
+						this.canNext = props.canNext;
+						this.startRow = props.startRow;
+						this.endRow = props.endRow;
+						this.lastIndex = props['resolvedData'].length - 1;
+						return {};
+					}}
 					getTrProps={(state, rowInfo) => {
 						let classes = [];
 						if (rowInfo) {
 							if (rowInfo['original']['lyrics'] !== null) {
 								classes.push('rt-tr--has-lyrics');
 							}
-							if (rowInfo['original']['id'] === this.state.selectedId) {
+							if (rowInfo['index'] === this.state.selectedIndex) {
 								classes.push('rt-tr--selected');
 							}
 						}
@@ -147,10 +255,10 @@ export default class FileList extends Component {
 						return {
 							className: classes,
 							onClick: () => {
-								if (rowInfo && rowInfo['original']) {
+								if (rowInfo) {
 									void this.props.songSelected(rowInfo['original']);
-									if (rowInfo['original']['id']) {
-										this.setState({selectedId: rowInfo['original']['id']});
+									if (rowInfo['index']) {
+										this.setState({selectedIndex: rowInfo['index']});
 									}
 								}
 							}
